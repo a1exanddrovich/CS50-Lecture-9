f2c030b7c17be6330ac33e4a684ce00e9512bbf8
Alexey Bliznichenko <l.bliznichenko@mail.ru>
2022-05-18 07:44:58 +0300

    Validation added, some forms added, 'DB' as Global variable added

--- app.py
+++ app.py
@@ -2,16 +2,43 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+REGISTRANTS = {}
+
+SPORTS = [
+    "Basketball",
+    "Soccer",
+    "Other"
+]
+
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("form_index.html", sports=SPORTS)
 
 
 @app.route("/greet")
 def greet():
     name = request.args.get("name", "world")
     return render_template("greet.html", name=name)
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    name = request.form.get("name")
+    sport = request.form.get("sport")
+
+    # Validate input
+    if not name or not sport or sport not in SPORTS:
+        return render_template("failure.html")
+
+    REGISTRANTS[name] = sport
+
+    return render_template("success.html")
+
+
+@app.route("/registrants")
+def registrants():
+    return render_template("registrants.html", registants=REGISTRANTS)
 
 
 if __name__ == '__main__':
